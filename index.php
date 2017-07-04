892f08908194a6ca4fd6acaa7daae7bbbeda109c
Zámbó Sándor Dávid <david.a.zambo@gmail.com>
2017-07-04 08:05:25 +0200

    Mobile view implemented, foggy weather included, night mode in

--- index.php
+++ index.php
@@ -17,12 +17,16 @@
     <!-- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+    <!-- Tether.io -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/css/tether.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.js"></script>
+
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
     <!-- CSS -->
-    <link rel="stylesheet" media="screen" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 
     <!-- Animate CSS -->
     <link rel="stylesheet" href="css/animate.css">
@@ -35,25 +39,25 @@
     </div>
     <div class="container-fluid">
       <div class="row content">
-        <div class="col-md-4 offset-sm-4 col-xs-10 offset-xs-1">
+        <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
 
           <div class="row">
             <div class="col-xs-12 col-md-12 text-right">
               <div id="country"></div>
             </div>
           </div>
-          <div class="row">
+          <div class="row city-container">
             <div class="col-4">
               <div id="weather-main-icon" class="text-center"></div>
             </div>
             <div class="col-8">
-              <div id="city"></div>
+              <div id="city" class="text-right"></div>
             </div>
           </div>
 
           <div class="row" id="temperature">
             <div class="col-2 text-left">
-              <div id="temp-icon" class="text-center"><i class="wi wi-thermometer-internal"></i></div>
+              <div id="temp-icon" class="text-center"><i class="wi wi-thermometer"></i></div>
             </div>
             <div class="col-4 text-center">
               <div class="temperature" id="temperatureFahrenheit"></div>
@@ -164,6 +168,11 @@ $(document).ready(function(){
                   weatherIcon = "<i class='wi wi-snow'></i>";
                   $('body').css('background-image', 'url(images/snow.jpg)');
                   break;
+            case (weatherId > 699 && weatherId < 782):
+                  weatherIcon = "<i class='wi wi-fog'></i>";
+                  $('body').css('background-image', 'url(images/foggy.jpg)');
+                  break;
+
             case (weatherId === 800):
                   weatherIcon = "<i class='wi wi-day-sunny'></i>";
                   if (celsiusValue > 35) {
@@ -177,7 +186,8 @@ $(document).ready(function(){
                   $('body').css('background-image', 'url(images/warm-cloudy.jpg)');
                   break;
             default:
-                    weatherIcon = "?";
+                  weatherIcon = "<i class='wi wi-na'></i>";
+                  $('body').css('background-color', 'white');
           }
 
           $('#weather-main-icon').html(weatherIcon);
@@ -200,6 +210,11 @@ $(document).ready(function(){
 
           $('#sunrise-time').html(addZero(sunrise.getHours())+":"+addZero(sunrise.getMinutes()));
           $('#sunset-time').html(addZero(sunset.getHours())+":"+addZero(sunset.getMinutes()));
+
+        //Set night background-image
+          if (daytime > sunset){
+            $('body').css('background-image', 'url(images/night.jpg)');
+          }
 
           $('#loadingMessage').hide().addClass('animated fadeOut');
           $('.content').show().addClass('animated fadeInDownBig');
